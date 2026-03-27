6948dd85d87a1783d0adac9be00c2a3a91167146
Pedro Luiz <pedroluizsousasantos@gmail.com>
2026-03-27 14:35:47 -0300

    Dei update na descriçao do express
    adicionei uma variavel resposta com enunciado dentro

--- index.js
+++ index.js
@@ -13,47 +13,92 @@ const app = express()
 
 app.get('/ex1', (req, res) => {
     let ex1 = objCasa()
-  res.send(ex1)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex1
+
+    }
+    res.json(resposta)
 })
 
 app.get('/ex2', (req, res) => {
     let ex2 =  decVariavel()
-  res.send(ex2)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex2
+
+    }
+  res.json(resposta)
 })
 
 app.get('/ex3', (req, res) => {
     let ex3 = opeAritmeticas()
-  res.send(ex3)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex3
+
+    }
+  res.json(resposta)
 })
 
 app.get('/ex4', (req, res) => {
     let ex4 = opeRelacionais()
-  res.send(ex4)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex4
+
+    }
+  res.json(resposta)
 })
 
 app.get('/ex5', (req, res) => {
     let ex5 = opeLogicas()
-  res.send(ex5)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex5
+
+    }
+  res.json(resposta)
 })
 
 app.get('/ex6', (req, res) => {
     let ex6 = ifElse()
-  res.send(ex6)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex6
+
+    }
+  res.json(resposta)
 })
 
 app.get('/ex7', (req, res) => {
     let ex7 = Case()
-  res.send(ex7)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex7
+
+    }
+  res.json(resposta)
 })
 
 app.get('/ex8', (req, res) => {
     let ex8 = laçoRep()
-  res.send(ex8)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex8
+
+    }
+  res.json(resposta)
 })
 
 app.get('/ex9', (req, res) => {
     let ex9 = Vetor()
-  res.send(ex9)
+    let resposta = {
+        enunciado: 'asdfad',
+        resultado: ex9
+
+    }
+  res.json(resposta)
 })
 
 
