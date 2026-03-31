17d3a2ffc4aac4bf8b300fe004721fa70ec1bf03
Pedro Luiz <pedroluizsousasantos@gmail.com>
2026-03-30 22:55:47 -0300

    Adicionei as descriçoes sobre as funçoes e adicionei uma pagina html

--- ex2.js
+++ ex2.js
@@ -2,7 +2,7 @@ function decVariavel (){
     let nome = "Maria";
     let idade = 25;
     const pais = "Brasil"
-    return nome + idade + pais;
+    return nome + idade + pais ;
 }
 decVariavel()
 export {decVariavel}
